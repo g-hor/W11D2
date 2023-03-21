c979d4cd90596645f7664625eb4e7b6aff5c86df
Gary <geehor@gmail.com>
2023-03-21 15:22:12 -0400

    Start Folder refactor

---
+++ practice-for-week-15-react-refactoring-class-components-long-practice/src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import Clock, { ClockToggle } from './components/Clock';
+import Folder from './components/Folder';
+import Weather from './components/Weather';
+import Autocomplete from './components/Autocomplete';
+import { useState } from 'react';
+
+const App = (props) => {
+  const [showClock, setShowClock] = useState(true);
+
+  const names = [
+    'Abba',
+    'Barbara',
+    'Barney',
+    'Jeff',
+    'Jenny',
+    'Sally',
+    'Sarah',
+    'Xander'
+  ];
+  
+  const folders = [
+    { title: 'one', content: 'I am the first' },
+    { title: 'two', content: 'Second folder here' },
+    { title: 'three', content: 'Third folder here' }
+  ];
+
+  const toggleClock = (e) => {
+    e.preventDefault();
+    setShowClock((prevState) => (!prevState));
+  }
+
+  return (
+    <div className="widgets">
+      <Folder folders={folders} />
+      <Weather />
+      <ClockToggle toggleClock={toggleClock} />
+      {showClock && <Clock />}
+      <Autocomplete names={names} />
+    </div>
+  )
+}
+
+export default App;
---
+++ practice-for-week-15-react-refactoring-class-components-long-practice/src/components/Folder.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+
+
+const Headers = ({ titles, currentTab, selectTab }) => {
+  const handleClick = (e) => {
+    const idx = parseInt(e.target.id, 10);
+    selectTab(idx);
+  }
+
+  const tabs = titles.map((title, idx) => {
+    const headerClass = (idx === currentTab) ? 'active' : '';
+
+    return (
+      <li
+        key={idx}
+        id={idx}
+        onClick={handleClick}
+        className={headerClass}
+      >
+        {title}
+      </li>
+    );
+  });
+}
+
+const Folder = (props) => {
+  const [currentTab, setCurrentTab] = useState(0);
+
+  const selectTab = (num) => {
+    setCurrentTab(num);
+  }
+
+  const folder = props.folders[currentTab];
+  const titles = props.folders.map((folder) => folder.title);
+
+  return (
+    <section className="tabs-section">
+      <h1>Tabs</h1>
+      <div className='tabs'>
+        <Headers
+          titles={titles}
+          currentTab={currentTab}
+          selectTab={selectTab}
+        />
+        <div className='tab-content'>
+          {folder.content}
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Folder;
